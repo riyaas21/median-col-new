dccef3f3919700adf26865e15d748617bb37ec1c
riyaas21 <90369888+riyaas21@users.noreply.github.com>
2023-05-08 13:32:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import cv2
 
 def test_webcam():
-    cap = cv2.VideoCapture("VID2.mp4") # replace with the path to your pre-recorded video file
+    cap = cv2.VideoCapture(0) # replace with the path to your pre-recorded video file
     assert cap.isOpened() == True # check if the camera is opened successfully
 
     while cap.isOpened():
