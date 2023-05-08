c5cd71901aa0d6ffff2f47e90ead3a13b844d02e
riyaas21 <90369888+riyaas21@users.noreply.github.com>
2023-05-08 13:19:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,8 @@
 import cv2
 
-def test_webcam_working():
-    cap = cv2.VideoCapture(0)
-    assert cap.isOpened(), "Webcam is not working"
-
-def test_webcam_capture():
-    cap = cv2.VideoCapture(0)
-    assert cap.isOpened(), "Webcam is not working"
-    
-    ret, frame = cap.read()
-    assert ret, "Failed to capture frame from webcam"
-
-def test_webcam_release():
-    cap = cv2.VideoCapture(0)
-    assert cap.isOpened(), "Webcam is not working"
-    
-    cap.release()
-    assert not cap.isOpened(), "Failed to release webcam"
-
-def test_webcam_resolution():
-    cap = cv2.VideoCapture(0)
-    assert cap.isOpened(), "Webcam is not working"
-    
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    
-    assert width > 0, "Failed to retrieve webcam resolution (width)"
-    assert height > 0, "Failed to retrieve webcam resolution (height)"
+def test_webcam():
+    cap = cv2.VideoCapture(0) # 0 is for the default webcam, change it if necessary
+    assert cap.isOpened() == True # check if the camera is opened successfully
+    ret, frame = cap.read() # read the first frame
+    assert ret == True # check if the frame is read successfully
+    cap.release() # release the camera
