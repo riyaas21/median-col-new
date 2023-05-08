b785d026e0d2d278f27620fef11aaed82e258a85
riyaas21 <90369888+riyaas21@users.noreply.github.com>
2023-05-08 13:25:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import cv2
 
 def test_webcam():
-    cap = cv2.VideoCapture(0) # 0 is for the default webcam, change it if necessary
+    cap = cv2.VideoCapture("VID2.mp4") # replace with the path to your pre-recorded video file
     assert cap.isOpened() == True # check if the camera is opened successfully
-    ret, frame = cap.read() # read the first frame
-    assert ret == True # check if the frame is read successfully
-    cap.release() # release the camera
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
